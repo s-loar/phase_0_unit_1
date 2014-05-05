efaeb04989cc8c0348874e6ab935e89158a6423c
Steve Loar <sjl7909@gmail.com>
2014-05-05 14:53:22 -0400

    week 2 Unit 3 my_solution.js

--- week_3/3_js_variables_objects/my_solution.js
+++ week_3/3_js_variables_objects/my_solution.js
@@ -4,15 +4,20 @@
 
 
 // Pseudocode
-// 
-// 
-// 
-// 
+// define secretNumber
+// give it a numeric value
+// define password
+// give it a string value
+// define members as an array
+// give it some string values
 
 
 // __________________________________________
 // Write your code below.
-
+var secretNumber = 7;
+var password = "just open the door";
+var allowedIn = false;
+var members = ["John","Teri","William","Mary"];
 
 
 
@@ -21,17 +26,19 @@
 
 // __________________________________________
 // Refactored Code: Include a refactored version (or justification of your original code) here. 
-
+// Not sure that this needs refactoring as it is fairly simple. There are just variables and their
+// initial values. I did this by writing out hte pseudocode, then running the file in node and 
+// then adding things to the code to pass each test.
 
 
 
 
 // __________________________________________
 // Reflection: Use the reflection guidelines to write a reflection here. 
-// 
-// 
-// 
-// 
+// For this I wrote the pseudo code based on the test that were to be run against the code.
+// Then I ran the solution in node to see what happened. It failed. So I had fun just adding
+// what the test said it needed, and built the program that way. There were no questions or 
+// issues in building this. So I feel good in having learned this compentency.
 // 
 // 
 

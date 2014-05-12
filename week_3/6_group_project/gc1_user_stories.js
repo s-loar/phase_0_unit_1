d35d61554f7db1bcc75caf5666f7699fa1d2d769
Steve Loar <sjl7909@gmail.com>
2014-05-11 21:25:02 -0400

    Week #3 Unit 6 Reflection added

--- week_3/6_group_project/gc1_user_stories.js
+++ week_3/6_group_project/gc1_user_stories.js
@@ -7,3 +7,12 @@
 // I want to get the median of a set of numbers
 
 
+// __________________________________________
+// Reflection: Use the reflection guidelines
+// It was difficult to try and tell in simple non-technical phrases what was needed to get the test to work.
+// I tried to use the function name in the sentence and use the words "set of numbers" to mean array. I do 
+// not think that translated as well as it should. I would like to know more about making user stories that
+// translate better into acutal code. I did like getting back the final set of code and seeing what it had
+// become. It was like the telephone game. We only had one error in the final version, and it was kinda right
+// but we ended up with a float for the array index instead of an integer. 
+
